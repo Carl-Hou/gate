5d5d5b7168cabf1e588b32a4c11ee727c3678fc3
Carl-Hou <m2houzhao@outlook.com>
2020-06-14 23:16:43 +0800

    Add Element for upload file via http

---
+++ core/src/main/java/org/gate/gui/graph/elements/sampler/protocol/http/HttpFileUploader.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.gate.gui.graph.elements.sampler.protocol.http;
+
+import org.apache.http.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.FormBodyPart;
+import org.apache.http.entity.mime.FormBodyPartBuilder;
+import org.apache.http.entity.mime.MIME;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.gate.gui.graph.elements.sampler.protocol.http.gui.HttpFileUploaderGui;
+import org.gate.varfuncs.property.GateProperty;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class HttpFileUploader extends HTTPHCAbstractImpl {
+
+    public static final String PN_FilePath = "file_path";
+    public static final String PN_ParameterName = "parameter_nam";
+    public static final String PN_MIMEType = "mime_type";
+    public static final String PN_BrowserCompatibleHeaders = "browser-compatible_headers";
+
+    public HttpFileUploader() {
+    }
+
+    @Override
+    void addProps(){
+        addProp(NS_DEFAULT, PN_FilePath, "");
+        addProp(NS_DEFAULT, PN_ParameterName, "");
+        addProp(NS_DEFAULT, PN_MIMEType, "");
+        addProp(NS_DEFAULT, PN_BrowserCompatibleHeaders, "false");
+    }
+
+    @Override
+    public String getGUI() {
+        return HttpFileUploaderGui.class.getName();
+    }
+
+    /**
+     * @param post {@link HttpPost}
+     * @return String posted body if computable
+     * @throws IOException if sending the data fails due to I/O
+     */
+    protected String sendPostData(HttpPost post) throws IOException {
+        // Buffer to hold the post body, except file content
+        StringBuilder postedBody = new StringBuilder(1000);
+        String contentEncoding = getRunTimeProp(NS_DEFAULT, PN_ContentEncoding);
+        final boolean haveContentEncoding = !contentEncoding.isEmpty();
+        Charset charset = null;
+        if(haveContentEncoding) {
+            charset = Charset.forName(contentEncoding);
+        } else {
+            charset = MIME.DEFAULT_CHARSET;
+        }
+        MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create()
+                .setCharset(charset);
+        if(Boolean.parseBoolean(getRunTimeProp(NS_DEFAULT, PN_BrowserCompatibleHeaders))){
+            multipartEntityBuilder.setLaxMode();
+        }else{
+            multipartEntityBuilder.setStrictMode();
+        }
+
+        for (GateProperty argument : getRunTimeProps(NS_ARGUMENT)) {
+            if (argument.getName().trim().isEmpty()) {
+                continue;
+            }
+            StringBody stringBody = new StringBody(argument.getStringValue(), ContentType.create("text/plain", charset));
+            FormBodyPart formPart = FormBodyPartBuilder.create(argument.getName(), stringBody).build();
+            multipartEntityBuilder.addPart(formPart);
+        }
+        FileBody fileBody = new FileBody(new File(getRunTimeProp(NS_DEFAULT, PN_FilePath)), getRunTimeProp(NS_DEFAULT, PN_MIMEType));
+        multipartEntityBuilder.addPart(getRunTimeProp(NS_DEFAULT, PN_ParameterName), fileBody);
+
+        HttpEntity entity = multipartEntityBuilder.build();
+        post.setEntity(entity);
+        if (entity.isRepeatable()){
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            entity.writeTo(bos);
+            bos.flush();
+            // We get the posted bytes using the encoding used to create it
+            postedBody.append(bos.toString(
+                    contentEncoding.isEmpty() ? "US-ASCII" // $NON-NLS-1$ this igetSendParameterValuesAsPostBodys the default used by HttpClient
+                            : contentEncoding));
+            bos.close();
+        } else {
+            postedBody.append("<Multipart was not repeatable, cannot view what was sent>"); // $NON-NLS-1$
+        }
+        return postedBody.toString();
+    }
+
+    @Override
+    public String getStaticLabel() {
+        return "Http File Upload";
+    }
+
+
+
+}
--- core/src/main/java/org/gate/gui/graph/elements/sampler/protocol/http/gui/HttpFileUploaderGui.java
+++ core/src/main/java/org/gate/gui/graph/elements/sampler/protocol/http/gui/HttpFileUploaderGui.java
@@ -21,9 +21,7 @@ import com.mxgraph.model.mxCell;
 import com.mxgraph.view.mxGraph;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.gate.common.config.GateProps;
 import org.gate.gui.GuiPackage;
-import org.gate.gui.common.OptionPane;
 import org.gate.gui.common.GuiUtils;
 import org.gate.gui.common.TestElement;
 import org.gate.gui.common.VerticalLayout;
@@ -34,40 +32,26 @@ import org.gate.gui.details.properties.graph.GraphElementPropertiesGui;
 import org.gate.gui.details.properties.graph.GraphNamePane;
 import org.gate.gui.graph.elements.GraphElement;
 import org.gate.gui.graph.elements.sampler.protocol.http.HTTPConstantsInterface;
-import org.gate.varfuncs.property.GateProperty;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
-import java.util.LinkedList;
 
 
-public class DefaultHttpElementGui extends JPanel implements GraphElementPropertiesGui, HTTPConstantsInterface {
+public class HttpFileUploaderGui extends JPanel implements GraphElementPropertiesGui, HTTPConstantsInterface {
 
     protected Logger log = LogManager.getLogger(this.getName());
-
-    private final static String Parameters = "Parameters";
-    private final static String BodyData = "Body Data";
-    private final static int ParametersIndex = 0;
-    private final static int BodyDataIndex = 1;
-
-    JComboBox methodsComboBox = new JComboBox(new String[]{
-            POST ,GET ,PUT ,HEAD ,TRACE ,OPTIONS ,DELETE ,PATCH});
 
     GraphNamePane namePane = new GraphNamePane();
     JTextField pathField = new JTextField();
     PropertiesTable defaultPropertiesTable = new PropertiesTable(new PropertiesTableModel());
-    JTabbedPane parametersTabbedPane = new JTabbedPane();
-//    PropertiesTable argumentPropertiesTable = new PropertiesTable(new PropertiesTableModel());
-    ArgumentsPane argumentsPane = new ArgumentsPane();
-    JTextArea bodyDataTextArea = new JTextArea();
+    JComboBox methodsComboBox = new JComboBox(new String[]{POST});
 
+    ArgumentsPane argumentsPane = new ArgumentsPane();
     GraphElement graphElement;
 
-    public DefaultHttpElementGui(){
+    public HttpFileUploaderGui(){
         setLayout(new VerticalLayout());
         add(namePane);
         pathField.getDocument().addDocumentListener(new DocumentListener() {
@@ -96,68 +80,10 @@ public class DefaultHttpElementGui extends JPanel implements GraphElementPropert
 
         propertiesParameters.setLeftComponent(propertiesPanel);
 
-        bodyDataTextArea.getDocument().addDocumentListener(new DocumentListener() {
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                updateText();
-            }
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                updateText();
-            }
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                updateText();
-            }
-            void updateText(){
-                String text = bodyDataTextArea.getText();
-                if(text.isEmpty()){
-                    graphElement.getProps(TestElement.NS_ARGUMENT).clear();
-                }else{
-                    graphElement.putProp(TestElement.NS_ARGUMENT, "", bodyDataTextArea.getText());
-                }
-
-            }
-        });
-        parametersTabbedPane.add(Parameters, argumentsPane);
-        parametersTabbedPane.add(BodyData, bodyDataTextArea);
-        parametersTabbedPane.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                int selectedIndex = parametersTabbedPane.getSelectedIndex();
-                if(selectedIndex == BodyDataIndex){
-                    if(graphElement.getProps(TestElement.NS_ARGUMENT).size() ==0){
-                        return;
-                    }
-                    if(!isBodyData()){
-                        OptionPane.showErrorMessageDialog("Warrning",
-                                "Can not convert parameters to Body Data" + GateProps.LineSeparator
-                                        + "because one of the parameters have a name");
-                        parametersTabbedPane.setSelectedIndex(ParametersIndex);
-                        argumentsPane.setTestElement(graphElement);
-                        return;
-                    }else if(graphElement.getProps(TestElement.NS_ARGUMENT).size() ==0){
-                        bodyDataTextArea.setText("");
-                    }
-                    bodyDataTextArea.setText(graphElement.getProps(TestElement.NS_ARGUMENT).getFirst().getStringValue());
-                }else{
-                    argumentsPane.setTestElement(graphElement);
-                }
-
-            }
-        });
-        propertiesParameters.setRightComponent(parametersTabbedPane);
+        propertiesParameters.setRightComponent(argumentsPane);
         int divider = (int) (GuiPackage.getIns().getPropertiesPanel().getWidth() *0.4);
         propertiesParameters.setDividerLocation(divider);
         add(propertiesParameters);
-    }
-
-    boolean isBodyData(){
-        LinkedList<GateProperty> arguments = graphElement.getProps(TestElement.NS_ARGUMENT);
-        if(arguments.size() == 1 && arguments.getFirst().getName().isEmpty()){
-            return true;
-        }
-        return false;
     }
 
     @Override
@@ -170,20 +96,9 @@ public class DefaultHttpElementGui extends JPanel implements GraphElementPropert
         updateParamterTabbedPane();
     }
 
-
-
     void updateParamterTabbedPane() {
-        if (isBodyData()) {
-            bodyDataTextArea.setText(graphElement.getProps(TestElement.NS_ARGUMENT).getFirst().getStringValue());
-            parametersTabbedPane.setSelectedIndex(BodyDataIndex);
-        }else{
-            bodyDataTextArea.setText("");
-            parametersTabbedPane.setSelectedIndex(ParametersIndex);
-        }
-
         defaultPropertiesTable.setComboBox(PN_Method, methodsComboBox);
         defaultPropertiesTable.setBooleanOnCell(PN_UseKeepAlive);
-
     }
 
 }
